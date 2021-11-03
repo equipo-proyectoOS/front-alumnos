cbb11cfff8660f53e6e11a49a2415f69adecb5d3
santiR22 <santiagorodas688@gmail.com>
2021-11-03 13:56:31 -0300

    update

--- src/components/layout/AñadirDatos.jsx
+++ src/components/layout/AñadirDatos.jsx
@@ -9,94 +9,157 @@ import "../../Login_v4/vendor/select2/select2.min.css";
 import "../../Login_v4/vendor/daterangepicker/daterangepicker.css";
 import "../../Login_v4/css/util.css";
 import "../../Login_v4/css/main.css";
-import styles from "../layout/AñadirDatos.module.css"
+import imagen from "../../Login_v4/images/bg-01.jpg";
+import styles from "../layout/AñadirDatos.module.css";
 
 export default function AñadirDatos() {
   return (
-    <form class="login100-form validate-form">
-      <div class="wrap-input100 validate-input m-b-23">
-        <span class="login100-form-title p-b-49">Información personal</span>
-        <input
-          class={`input100 ${styles.input}`}
-          type="text"
-          name=""
-          placeholder="nombre completo"
-        />
-        <input
-          class="input100"
-          type="text"
-          name=""
-          placeholder="fecha de nacimiento"
-        />
-        <input class="input100" type="text" name="" placeholder="dni" />
-        <input class="input100" type="text" name="" placeholder="genero" />
-        <input class="input100" type="text" name="" placeholder="pais" />
-        <input class="input100" type="text" name="" placeholder="provincia" />
-        <input class="input100" type="text" name="" placeholder="direccion" />
-        <input class="input100" type="text" name="" placeholder="aficiones" />
-        <span class="focus-input100" data-symbol="&#xf206;"></span>
-      </div>
-      <div class="wrap-input100 validate-input m-b-23">
-        <span class="login100-form-title p-b-49">Informacion de contacto</span>
-        <input class="input100" type="text" name="" placeholder="Telefono" />
-        <input
-          class="input100"
-          type="text"
-          name=""
-          placeholder="Correo electronico"
-        />
-        <input
-          class="input100"
-          type="text"
-          name=""
-          placeholder="Redes Sociales"
-        />
-        <span class="focus-input100" data-symbol="&#xf206;"></span>
-      </div>
-      <div class="wrap-input100 validate-input m-b-23">
-        <span class="login100-form-title p-b-49">Informacion académica</span>
-        <input
-          class="input100"
-          type="text"
-          name=""
-          placeholder="Nivel academico"
-        />
-        <input
-          class="input100"
-          type="text"
-          name=""
-          placeholder="Licenciaturas"
-        />
-        <input
-          class="input100"
-          type="text"
-          name=""
-          placeholder="Certificados"
-        />
-        <span class="focus-input100" data-symbol="&#xf206;"></span>
-      </div>
-      <div
-        class="wrap-input100 validate-input m-b-23"
-        data-validate="Usuario es requerido"
-      >
-        <span class="login100-form-title p-b-49">Informacion profesional</span>
-        <input class="input100" type="text" name="" placeholder="Resumen" />
-        <input
-          class="input100"
-          type="text"
-          name=""
-          placeholder="Experiencia Laboral"
-        />
-        <input class="input100" type="text" name="" placeholder="Habilidades" />
-        <input class="input100" type="text" name="" placeholder="Lenguajes" />
-        <span class="focus-input100" data-symbol="&#xf206;"></span>
-      </div>
-      <div class="container-login100-form-btn">
-        <div class="wrap-login100-form-btn w-auto">
-          <div class="login100-form-bgbtn"></div>
-          <button class="login100-form-btn">Guardar Registro</button>
+    <div class="limiter">
+        <div
+          class="container-login100 container-fluid"
+          style={{ backgroundImage: `url(${imagen})` }}
+        >
+          <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+            <form class="login100-form validate-form">
+              <div class="wrap-input100 validate-input m-b-23">
+                <span class="login100-form-title p-b-49">
+                  Información personal
+                </span>
+                <input
+                  class={`input100 ${styles.input}`}
+                  type="text"
+                  name=""
+                  placeholder="nombre completo"
+                />
+                <input
+                  class="input100"
+                  type="date"
+                  name=""
+                  placeholder="fecha de nacimiento"
+                />
+                <input class="input100" type="text" name="" placeholder="dni" />
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="genero"
+                />
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="pais"
+                />
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="provincia"
+                />
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="direccion"
+                />
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="aficiones"
+                />
+                <span class="focus-input100" data-symbol="&#xf206;"></span>
+              </div>
+              <div class="wrap-input100 validate-input m-b-23">
+                <span class="login100-form-title p-b-49">
+                  Informacion de contacto
+                </span>
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="Telefono"
+                />
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="Correo electronico"
+                />
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="Redes Sociales"
+                />
+                <span class="focus-input100" data-symbol="&#xf206;"></span>
+              </div>
+              <div class="wrap-input100 validate-input m-b-23">
+                <span class="login100-form-title p-b-49">
+                  Informacion académica
+                </span>
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="Nivel academico"
+                />
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="Licenciaturas"
+                />
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="Certificados"
+                />
+                <span class="focus-input100" data-symbol="&#xf206;"></span>
+              </div>
+              <div
+                class="wrap-input100 validate-input m-b-23"
+                data-validate="Usuario es requerido"
+              >
+                <span class="login100-form-title p-b-49">
+                  Informacion profesional
+                </span>
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="Resumen"
+                />
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="Experiencia Laboral"
+                />
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="Habilidades"
+                />
+                <input
+                  class="input100"
+                  type="text"
+                  name=""
+                  placeholder="Lenguajes"
+                />
+                <span class="focus-input100" data-symbol="&#xf206;"></span>
+              </div>
+            </form>
+            <div class="container-login100-form-btn">
+              <div class="wrap-login100-form-btn w-auto">
+                <div class="login100-form-bgbtn"></div>
+                <button class="login100-form-btn">Guardar Registro</button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </form>
+    </div>
   );
 }
